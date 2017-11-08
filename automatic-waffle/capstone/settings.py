d21ca36c5e27996391769b4741a59ef90bb11bf2
Azenwrath <dana.stubkjaer@gmail.com>
2017-11-08 10:00:29 -0800

    Removing development settings for static files

--- automatic-waffle/capstone/settings.py
+++ automatic-waffle/capstone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = tweets.config.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aws-capstone2-dev.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['aws-capstone2-dev.us-west-2.elasticbeanstalk.com', 'localhost']
 
 
 # Application definition
