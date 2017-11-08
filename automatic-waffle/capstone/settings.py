eda4680b187a3ed72e4ed5e2f0d04920f87aeb07
Azenwrath <dana.stubkjaer@gmail.com>
2017-11-08 10:35:56 -0800

    Adjusting database settings

--- automatic-waffle/capstone/settings.py
+++ automatic-waffle/capstone/settings.py
@@ -68,35 +68,35 @@ TEMPLATES = [
         },
     },
 ]
-
+on
 WSGI_APPLICATION = 'capstone.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
+# if 'RDS_DB_NAME' in os.environ:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'capstone',
-            'USER': 'azenwrath',
-            'PASSWORD': tweets.config.DATA_KEY,
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+}
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': 'capstone',
+#             'USER': 'azenwrath',
+#             'PASSWORD': tweets.config.DATA_KEY,
+#             'HOST': 'localhost',
+#             'PORT': '',
+#         }
+#     }
 
 
 # Password validation
