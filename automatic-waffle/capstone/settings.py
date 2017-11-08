b3fe7c40ab1f517674533b879cef54ba7a9bd0ef
Azenwrath <dana.stubkjaer@gmail.com>
2017-11-08 10:08:18 -0800

    adjusting relative file paths

--- automatic-waffle/capstone/settings.py
+++ automatic-waffle/capstone/settings.py
@@ -137,9 +137,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", 'static')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'tweets', 'static')]
+
 
 
 
