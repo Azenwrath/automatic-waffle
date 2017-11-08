2f8e766e286b7e09a6c087d6657045189b9ce995
Azenwrath <dana.stubkjaer@gmail.com>
2017-11-08 11:40:25 -0800

    removing verbose testing leftovers

--- automatic-waffle/tweets/tweet_search.py
+++ automatic-waffle/tweets/tweet_search.py
@@ -5,7 +5,7 @@ from twitter import Twitter, OAuth, TwitterHTTPError
 
 #
 def pull_tweets():
-    pass
+
     package = {}
     package['APPID'] = tweets.config.keys
 
@@ -15,17 +15,9 @@ def pull_tweets():
     CONSUMER_SECRET = tweets.config.CONSUMER_SECRET
 
     oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
-    # req = requests.post('http://api.openweathermap.org/data/2.5/weather?', params=package)
-    # pyobj = req.json()
-    # pyobj = json.loads(req.content)
+
     twitter = Twitter(auth=oauth)
     newtweets = twitter.search.tweets(q="#wafflehouse", count=100)
-    tweetport = json.dumps(newtweets, indent=4)
-
-    with open("tweets.txt", "w") as tweetfile:
-        for line in tweetport:
-            tweetfile.write(line)
-
     return newtweets
 
 
@@ -34,42 +26,3 @@ def read_tweets():
     with open("tweets.txt", 'r') as tweetfile:
         parsed_tweets = json.load(tweetfile)
         return parsed_tweets
-
-#
-#
-# # strippedtweets = json.load(tweets)
-# print(tweets)
-#
-# # print json.dumps(tweets, indent=4)
-# #
-# tweetport = json.dumps(tweets, indent=4)
-#
-#
-# with open("tweets.txt", "w") as tweetfile:
-#
-#     for line in tweetport:
-#         tweetfile.write(line)
-#
-#
-# # for i in tweets:
-# #     try:
-# #         tweet = json.loads(i.strip())
-# #         print(i)
-# #         print("Tweet: ", tweet)
-# #         if 'text' in tweet:
-# #             print tweet['id']  # This is the tweet's id
-# #             print tweet['created_at']  # when the tweet posted
-# #             print tweet['text']  # content of the tweet
-# #
-# #             print tweet['user']['id']  # id of the user who posted the tweet
-# #             print tweet['user']['name']  # name of the user, e.g. "Wei Xu"
-# #             print tweet['user']['screen_name']  # name of the user account, e.g. "cocoweixu"
-# #
-# #             hashtags = []
-# #             for hashtag in tweet['entities']['hashtags']:
-# #                 hashtags.append(hashtag['text'])
-# #             print hashtags
-# #
-# #     except:
-# #         # read in a line is not in JSON format (sometimes error occured)
-# #         continue
