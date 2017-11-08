f567442eb77a20b7bf2c89e91834636f348dc752
Azenwrath <dana.stubkjaer@gmail.com>
2017-11-08 09:42:14 -0800

    configuring static

--- automatic-waffle/capstone/settings.py
+++ automatic-waffle/capstone/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", 'static_root')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'tweets', 'static')]
 
