98a9bf4f8c34a7229effc812d4ab2908e153ac9f
Azenwrath <dana.stubkjaer@gmail.com>
2017-11-15 12:15:59 -0800

    tweaking deployment settings

--- automatic-waffle/capstone/settings.py
+++ automatic-waffle/capstone/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['DJANGO_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['aws-capstone2-dev.us-west-2.elasticbeanstalk.com', 'localhost', '54.148.95.8']
 
