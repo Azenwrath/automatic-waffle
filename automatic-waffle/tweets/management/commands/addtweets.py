505d83eb53703a2c58430ef2390df72b39ae1075
Azenwrath <dana.stubkjaer@gmail.com>
2017-11-16 13:50:55 -0800

    added minimal logging for tweet scraping

--- automatic-waffle/tweets/management/commands/addtweets.py
+++ automatic-waffle/tweets/management/commands/addtweets.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
 from tweets.tweet_search import add_tweets
-
+from datetime import datetime
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         add_tweets();
+        print('Scraped tweets at ', datetime.now())
 
 
