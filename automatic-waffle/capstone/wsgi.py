00daf930ac13568fe00e50de2274ed741d72d788
Azenwrath <dana.stubkjaer@gmail.com>
2018-02-12 14:06:58 -0800

    Updates WSGI for New Relic APM

--- automatic-waffle/capstone/wsgi.py
+++ automatic-waffle/capstone/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import newrelic.agent
+
+newrelic.agent.initialize('newrelic.ini')
 
 from django.core.wsgi import get_wsgi_application
+
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "capstone.settings")
 
 application = get_wsgi_application()
+application = newrelic.agent.wsgi_application()(application)
