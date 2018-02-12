4963a4bff23dc7e4f88b9913f4f9c8afdece9264
Azenwrath <dana.stubkjaer@gmail.com>
2018-02-12 14:35:40 -0800

    Changes WSGI.py to load New Relic APM in proper order

--- automatic-waffle/capstone/wsgi.py
+++ automatic-waffle/capstone/wsgi.py
@@ -6,11 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
-
-import os
 import newrelic.agent
+import os
 
-newrelic.agent.initialize('newrelic.ini')
+
+newrelic.agent.initialize('../newrelic.ini')
 
 from django.core.wsgi import get_wsgi_application
 
